5b1009a881b5457466c6da53ac73e538d67f6e06
Muqadas Nazif <135924883+Muqadasnaz12@users.noreply.github.com>
2026-02-09 20:33:54 -0500

    Add files via upload

---
+++ count1.s
@@ -0,0 +1,31 @@
+/*
+-------------------------------------------------------
+count1.s
+-------------------------------------------------------
+Author:  Muqadas Nazif
+ID:   169061218
+Email:  nazi1218@gmail.com
+Date:   2026-02-09
+-------------------------------------------------------
+A simple count down program (bgt)
+-------------------------------------------------------
+*/
+.org 0x1000  // Start at memory location 1000
+.text        // Code section
+.global _start
+_start:
+
+.text // code section
+// Store data in registers
+mov r3, #5  // Initialize a countdown value
+
+TOP:
+sub r3, r3, #1 // Decrement the countdown value
+cmp r3, #0  // Compare the countdown value to 0
+bge TOP         // Branch to TOP if > 0
+
+_stop:
+b _stop
+
+.end
+	
